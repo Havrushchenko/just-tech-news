2c3cb13125af19aac7f1f5f29d74ae733d2bc6fc
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-01-11 11:04:06 +0200

    created a partial for post information

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,5 +1,6 @@
 async function upvoteClickHandler(event) {
     event.preventDefault();
+
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -12,6 +13,7 @@ async function upvoteClickHandler(event) {
             'Content-Type': 'application/json'
         }
     });
+
     if (response.ok) {
         document.location.reload();
     } else {
